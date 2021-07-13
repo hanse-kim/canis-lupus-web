718fdb062bfad4a914884d6db956c97ef7c7bddf
bluewood <bluewood.truth@gmail.com>
2021-07-13 16:57:48 +0900

    build: add libraries
    - recoil: a state management library for React
    - chakra-ui: a simple, modular and accessible component library
    - react-query: performant and powerful data synchronization for React
    - immer: a library that makes it possible to work with immutable data
    - lodash: a library that makes JavaScript easier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query-devtools';
+import { RecoilRoot } from 'recoil';
+
+const queryClient = new QueryClient();
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
 }
+
 export default MyApp
