81061f193145bbaf2ce4b2e89ec0ac4c54d9299e
bluewood <bluewood.truth@gmail.com>
2021-07-17 10:13:28 +0900

    fix: add margin to bottom of Banner component

--- pages/common/components/Banner.tsx
+++ pages/common/components/Banner.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -11,19 +12,27 @@ const settings = {
   slidesToScroll: 1,
 };
 
+const SLICK_SLIDE_DOTS_HEIGHT = 28;
+
 const Banner = (props: {
   bannerItemComponent?: (props: any) => JSX.Element;
   bannerItemProps?: any[];
 }) => {
   return (
-    <Slider {...settings}>
-      {props.bannerItemProps?.map((bannerItemProp, index) => {
-        return (
-          props.bannerItemComponent &&
-            <props.bannerItemComponent key={index} {...bannerItemProp} />
-        );
-      })}
-    </Slider>
+    <Box
+      className="bannerWrapper"
+      marginBottom={`${SLICK_SLIDE_DOTS_HEIGHT}px`}
+    >
+      <Slider {...settings}>
+        {props.bannerItemProps?.map((bannerItemProp, index) => {
+          return (
+            props.bannerItemComponent && (
+              <props.bannerItemComponent key={index} {...bannerItemProp} />
+            )
+          );
+        })}
+      </Slider>
+    </Box>
   );
 };
 
