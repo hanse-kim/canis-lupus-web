147c09f6211899a127b9ef163a4a841c2ad77410
bluewood <bluewood.truth@gmail.com>
2021-07-19 14:02:09 +0900

    feat: add logic for search form
    Handle keyword value by react state.

--- pages/layout/Header.tsx
+++ pages/layout/Header.tsx
@@ -1,6 +1,7 @@
-import { Search2Icon } from "@chakra-ui/icons";
+import { CloseIcon, Search2Icon } from "@chakra-ui/icons";
 import {
   Box,
+  Button,
   Center,
   Container,
   Divider,
@@ -9,6 +10,7 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
+  InputRightElement,
   Link,
   useDisclosure,
 } from "@chakra-ui/react";
@@ -18,6 +20,15 @@ let LOGO_WIDTH = 74;
 
 const Header = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [keyword, setKeyword] = React.useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
+  };
+
+  const handleReset = () => {
+    setKeyword("");
+  }
 
   return (
     <header>
@@ -38,7 +49,7 @@ const Header = () => {
           </Link>
         </Box>
         <FormControl id="searchProductForm">
-          <InputGroup>
+          <InputGroup className="searchInput" maxWidth="container.sm">
             <InputLeftElement
               pointerEvents="none"
               color="gray.300"
@@ -47,10 +58,26 @@ const Header = () => {
             />
             <Input
               onClick={onOpen}
+              onChange={handleChange}
               variant="filled"
               borderRadius="9999"
               placeholder="찾으시는 모임이 있으신가요?"
-              maxWidth="container.sm"
+              value={keyword}
+            />
+            <InputRightElement
+              {...keyword === "" && {display: "none"}}
+              children={
+                <Button
+                  opacity="1"
+                  onClick={handleReset}
+                  borderRadius="9999"
+                  colorScheme="blackAlpha"
+                  variant="solid"
+                  size="xs"
+                >
+                  <CloseIcon boxSize="2" />
+                </Button>
+              }
             />
           </InputGroup>
         </FormControl>
