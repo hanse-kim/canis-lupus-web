7fcf0d52c8494169be2b069368d87833953470d4
bluewood <bluewood.truth@gmail.com>
2021-07-19 13:32:01 +0900

    feat: add global header
    Add logo and search form.

--- pages/layout/Header.tsx
+++ pages/layout/Header.tsx
@@ -1,5 +1,64 @@
+import { Search2Icon } from "@chakra-ui/icons";
+import {
+  Box,
+  Center,
+  Container,
+  Divider,
+  FormControl,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Link,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+
+let LOGO_WIDTH = 74;
+
 const Header = () => {
-  return <header>This is header</header>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <header>
+      <Container
+        className="headerWrapper"
+        display="flex"
+        maxWidth="container.lg"
+        marginY="6"
+      >
+        <Box
+          className="logoContainer"
+          marginRight="5"
+          height="auto"
+          marginY="auto"
+        >
+          <Link className="logoButton" href="/main">
+            <Image src="/logo.png" minWidth={`${LOGO_WIDTH}px`} />
+          </Link>
+        </Box>
+        <FormControl id="searchProductForm">
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              color="gray.300"
+              fontSize="1.2em"
+              children={<Search2Icon color="gray.500" />}
+            />
+            <Input
+              onClick={onOpen}
+              variant="filled"
+              borderRadius="9999"
+              placeholder="찾으시는 모임이 있으신가요?"
+              maxWidth="container.sm"
+            />
+          </InputGroup>
+        </FormControl>
+        <Center className="navContainer" justifyContent="end"></Center>
+      </Container>
+      <Divider />
+    </header>
+  );
 };
 
 export default Header;
