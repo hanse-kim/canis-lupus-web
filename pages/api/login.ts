c0ce90e585dd50a14cae444d3c08705e431ae39a
bluewood <bluewood.truth@gmail.com>
2021-07-23 16:56:17 +0900

    feat: temporarily implement login function

---
+++ pages/api/login.ts
@@ -0,0 +1,10 @@
+export const login = () => {
+  localStorage.setItem("user-token", "this is token");
+}
+
+export const checkLoggedIn = () => {
+  console.log(
+    "isLoggedIn: " + String(localStorage.getItem("user-token") !== null)
+  );
+  return localStorage.getItem("user-token") !== null;
+}
