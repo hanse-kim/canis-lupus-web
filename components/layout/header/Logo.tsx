8c8c244e2dbda78247d81da6da6e9604f8a52bfd
bluewood <bluewood.truth@gmail.com>
2021-08-11 20:03:43 +0900

    chore: change logo file .svg to .png

--- components/layout/header/Logo.tsx
+++ components/layout/header/Logo.tsx
@@ -1,12 +1,12 @@
 import {Box, Image, Link} from '@chakra-ui/react';
 
-const LOGO_WIDTH = 74;
+const LOGO_WIDTH = 162;
 
 const Logo = () => {
   return (
     <Box className="logoContainer" marginRight="5" height="auto" marginY="auto">
       <Link className="logoButton" href="/main">
-        <Image src="/logo.png" alt="logo" minWidth={`${LOGO_WIDTH}px`} />
+        <Image src="/logo.svg" alt="logo" minWidth={`${LOGO_WIDTH}px`} />
       </Link>
     </Box>
   );
