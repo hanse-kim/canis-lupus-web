96580a117d9adf014d87db424d7c520af6a51a19
bluewood <bluewood.truth@gmail.com>
2021-10-31 21:44:43 +0900

    chore: remove unused API

--- pages/api/login.ts
+++
@@ -1,7 +0,0 @@
-import {NextApiRequest, NextApiResponse} from 'next';
-
-const login = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({success: true, token: 'this_is_test_token'});
-};
-
-export default login;
--- pages/api/logout.ts
+++
@@ -1,7 +0,0 @@
-import {NextApiRequest, NextApiResponse} from 'next';
-
-const logout = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({success: true});
-};
-
-export default logout;
