f76ec04f2cf2430c333be4da3a0956d15c5385d2
bluewood <bluewood.truth@gmail.com>
2021-11-02 23:56:00 +0900

    refactor: add class name and arranged props

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,19 +1,12 @@
-import {
-  Box,
-  Container,
-  Divider,
-  Flex,
-  HStack,
-  Link,
-  Stack,
-} from '@chakra-ui/layout';
+import {Box, Divider, Flex, HStack, Link, Stack} from '@chakra-ui/layout';
+import Container from 'components/common/Container';
 import {colors} from 'style';
 
 const footerMenu = [
-  {title: '개인정보취급방침', fontWeight: 'bold', url: ''},
-  {title: '이용약관', fontWeight: 'medium', url: ''},
-  {title: '공지사항', fontWeight: 'medium', url: ''},
-  {title: '자주묻는 질문', fontWeight: 'medium', url: ''},
+  {title: '개인정보취급방침', url: ''},
+  {title: '이용약관', url: ''},
+  {title: '공지사항', url: ''},
+  {title: '자주 묻는 질문', url: ''},
 ];
 
 const corperation = '(주)이건모임';
@@ -36,13 +29,16 @@ const Footer = () => {
         className='footerWrapper'
         display='flex'
         flexDirection='column'
-        maxWidth='container.md'
         marginY='6'
       >
         <Flex minHeight='60px'>
-          <HStack spacing='23px'>
+          <HStack className='footerMenu' spacing='23px'>
             {footerMenu.map((item, index) => (
-              <Link key={index} href={item.url} fontWeight={item.fontWeight}>
+              <Link
+                key={index}
+                href={item.url}
+                fontWeight={index === 0 ? 'bold' : 'medium'}
+              >
                 {item.title}
               </Link>
             ))}
@@ -50,10 +46,10 @@ const Footer = () => {
         </Flex>
         <Divider borderColor={colors.mainGray[0]} />
         <Stack spacing='8px' paddingTop='40px' paddingBottom='96px'>
-          <Box fontSize='18px' fontWeight='bold'>
+          <Box className='corperation' fontSize='18px' fontWeight='bold'>
             {corperation}
           </Box>
-          <Stack color='#999' spacing='6px'>
+          <Stack className='info' color='#999' spacing='6px'>
             {footerInfo.map((item, index) => (
               <Box key={index}>{item}</Box>
             ))}
